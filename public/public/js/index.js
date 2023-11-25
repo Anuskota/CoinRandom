3e9780dab30bbe62c7f852f365704901999b5c77
Anuskota <anamangada.dev@gmail.com>
2023-11-25 23:19:33 +0100

    Creado JavaScript

--- public/public/js/index.js
+++ public/public/js/index.js
@@ -1,3 +1,6 @@
+/*Establecemos funciones con Id y Clases*/
 
+let coin = document.querySelector(".coin");
+let startBtn = document.querySelector("#start-button");
+let resetBtn = document.querySelector("#reset-button");
 
-let puntuaccion 
