e9e343b951241ab68b79e7ebacf8b411063df339
Anuskota <anamangada.dev@gmail.com>
2023-11-26 22:21:07 +0100

    decorando background

--- public/public/js/index.js
+++ public/public/js/index.js
@@ -1,35 +1,65 @@
-// /*Establecemos funciones con Id y Clases*/
 
-// let coin = document.querySelector(".coin");
-// let startBtn = document.querySelector("#start-button");
-// let resetBtn = document.querySelector("#reset-button");
+    
+
+
+        /*Establecemos funciones con Id y Clases*/
+
+
+        let coin = document.querySelector(".coin");
+        let flipBtn = document.querySelector("#flip-button");
+        let resetBtn = document.querySelector("#reset-button");
+
 
 // /*Inicializamos Heads y Tails a 0*/
+        let heads = 0;
+        let tails = 0;
 
-// let heads = 0;
-// let tails = 0;
+        flipBtn.addEventListener("click", () => {
+            let i = Math.floor(Math.random() * 2);
 
-// startBtn.addEventListener("click", () => {
-    
-//     /*Almaceno los valores en i*/
-//     /*Math.Random genera un numero aleatorio*/
-//     let i = Math.floor(Math.random() * 2);
+            coin.style.animation = "none";
+            console.log(flipBtn);
+            if (i) {
 
-//     coin.style.animation = "none";    
+                setTimeout(function(){
+                coin.style.animation = 'spin-heads 3s forwords ';   
+                }, 100);
+                heads++;
+            }else {
+                setTimeout(function(){
+                coin.style.animation = 'spin-tails 3s forwards';   
+                }, 100);
+                tails++;
 
-//     if (i) {
-//         setTimeout(function () {
-//             coin.style.animation = "spin-heads  3s forwords";
-//         }, 100);
-//         heads++;
-//     } else {
+            }
 
-//          setTimeout(function () {
-//             coin.style.animation = "spin-tails  3s forwords";
-//         }, 100);
-//         tails++;
+            setTimeout(updateStats, 3000);
+            disableButton();
 
-//     }
-//     setTimeout(updateStatds, 3000);// 1 seconds
-//     disableButton
-// })
+        })
+        function updateStats(){
+            document.querySelector("#heads-count").textContent = `Heads : ${heads}`;
+            document.querySelector("#tails-count").textContent = `Tails : ${tails}`;
+}
+        
+        function disableButton(){
+
+            flipBtn.disable = true;
+            setTimeout(function(){
+
+            flipBtn.disable = false;
+            },500);
+        }
+
+        /*Reset Button*/
+
+        resetBtn.addEventListener("click", () =>{
+
+            coin.style.animation="none";
+            heads=0;
+            tails=0;
+            updateStats();
+        })
+        
+        
+
